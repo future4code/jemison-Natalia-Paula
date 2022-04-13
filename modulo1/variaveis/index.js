12ef265eb34c06fb5a83cbc9c547a1b3896b3dc8
Natalia Dias Mendes De Paula <102389378+Nataliadmp@users.noreply.github.com>
2022-04-13 17:02:47 -0300

    exercicio-de-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,84 @@
+ /* Exercício nº 1 
+ let a = 10
+let b = 10
+
+console.log(b)
+10
+b = 5
+console.log(a, b)
+10,5
+*/
+
+/* Exercício nº 2
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) 
+(10,10,10_)*/
+
+ /* Exercício nº 3 fazemos as seguintes substituições
+
+let horasTrabalhada = prompt("Quantas horas você trabalha por dia?")
+let recebeAoDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${ recebeAoDia/horaTrabalhada} por hora`)
+*/
+
+let nome = natalia
+let idade =30
+
+
+console.log (typeof nome, idade);
+
+// Apareceu "undefined" pois não atribuimos valores".
+
+
+
+let nomeDoUsuario = prompt("Qual é o seu nome?");
+let idadeDoUsuario = prompt("informe sua idade?");
+
+// no console Apareceu as caixas de perguntas.
+
+
+console.log ("Olá nome, voce tem idade anos");
+
+ let bebeuAgua = prompt("você bebeu água hoje?") 
+ let exercicioAtrasado = prompt("entreguei exercício atrasado?") 
+ let horarioDaAula = prompt("minha aula é na parte da manhã?") 
+
+ console.log (typeof bebeuAgua);
+ console.log (typeof exercicioAtrasado);
+ console.log (typeof horarioDaAula);
+
+
+let a = 10
+let b = 25
+
+a = b 
+
+a = c
+b = a
+c = b 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
