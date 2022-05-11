937eef599555462b32ec301edaf56d84eaf589c5
Natalia Dias Mendes De Paula <102389378+Nataliadmp@users.noreply.github.com>
2022-05-10 22:03:11 -0300

    exercicio-de-revisao-js

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,106 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    const arrayTamanho = ["azul", "Amarelo", "vermelho"]
+    return arrayTamanho.length
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  const arrayReverse = [15,20,30,40,50]
+    return arrayReverse.reverse()
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  const arraycrescente =[2,9,7,5,62,30,22,18]
+    return arraycrescente.sort((a,b)=>a-b);
+}
+
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+    const numeros = [1,2,3,4,5,6,7,8,9,10]
+    const numerosPares = numeros.filter((numero)=>{
+    return (numero.indice % 2 == 0)
+    })
+  }
+console.log(numeros)
+
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+
+const retornarParesElevados =[2,4,5,6,7,8,9,10,]
+const numerosPares = retornarParesElevados.map((numeros)=>{
+    
+    return (numeros.indice % == 2(math.pow()
+})
+}
+console.log(retornarParesElevados)
+
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
+
